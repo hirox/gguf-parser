9831dba1400313de113c20be8733fbf63d97f36d
Hiromichi Goto <goto@suzuca.ai>
2024-05-07 00:31:43 +0900

    Fix license

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
